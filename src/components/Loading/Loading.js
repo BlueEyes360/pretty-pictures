86af2f43aaf318ac18a1381c2c525c5fba9300d5
Bobby <32147830+BlueEyes360@users.noreply.github.com>
2018-12-02 22:02:00 -0800

    Minor change to Loading CSS

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -6,8 +6,8 @@ const loading = (props) => {
 
     return (
         <div>
-            <img src={loadingsymbol} alt="Loading Symbol Slow Clockwise Spinning" className="SlowSpinning Loading" />
-            <img src={loadingsymbol} alt="Loading Symbol Fast Counter-Clockwise Spinning" className="FastSpinning Loading" />
+            <img src={loadingsymbol} alt="Loading Symbol Slow Clockwise Spinning" className="Loading" id="SlowSpinning" />
+            <img src={loadingsymbol} alt="Loading Symbol Fast Counter-Clockwise Spinning" className="Loading" id="FastSpinning" />
         </div>
     )
 }
